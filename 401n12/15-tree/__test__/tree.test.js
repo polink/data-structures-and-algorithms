37eadcd3d52931ba64c5d88964e91f8645a7b4aa
Karl Polintan <kf.polntan@gmail.com>
2019-06-08 16:35:47 -0700

    got tests done for BST, just need to finish one for contains() function

--- 401n12/15-tree/__test__/tree.test.js
+++ 401n12/15-tree/__test__/tree.test.js
@@ -64,7 +64,11 @@ describe('Binary Search Trees', () => {
     let bsTree = new BST();
     let bsRoot = new Node(15);
     bsTree.root = bsRoot;
-
+    let tree2 = new BST();
+    let tree2root = new Node(5);
+    tree2.root = tree2root;
+    tree2.add(tree2.root, 2);
+    tree2.add(tree2.root, 8);
     it('Correctly assigned BST nodes to their proper places', () => {
        bsTree.add(bsTree.root,20);
        bsTree.add(bsTree.root,10);
@@ -79,24 +83,22 @@ describe('Binary Search Trees', () => {
        expect(bsTree.root.left.left.value).toBe(7);
     });
     it('Can successfully add a left child and right child to a single root node', () => {
-        let tree2 = new BST();
-        let tree2root = new Node(5);
-        tree2.root = tree2root;
-        tree2.add(tree2.root, 2);
-        tree2.add(tree2.root, 8);
         expect(tree2.root.left.value).toBe(2);
         expect(tree2.root.right.value).toBe(8);
     });
     it('Can successfully return a collection from a preorder traversal', () => {
         // console.log(tree.preOrder());
-        expect(tree.preOrder()).toStrictEqual([1, 2, 4, 5, 3, 6]);
+        expect(tree2.preOrder()).toStrictEqual([5,2,8]);
     });
     it('Can successfully return a collection from an inorder traversal', () => {
         // console.log(tree.inOrder());
-        expect(tree.inOrder()).toStrictEqual([4, 2, 5, 1, 6, 3]);
+        expect(tree2.inOrder()).toStrictEqual([2,5,8]);
     });
     it('Can successfully return a collection from a postorder traversal', () => {
-        console.log(tree.postOrder());
-        expect(tree.postOrder()).toStrictEqual([4, 5, 2, 6, 3, 1]);
+        console.log(tree2.postOrder());
+        expect(tree2.postOrder()).toStrictEqual([2,8,5]);
     });
+    // it('Can use its contain function', () => {
+    //     expect(bsTree.contains(bsRoot, 7)).toEqual(true);
+    // });
 });
